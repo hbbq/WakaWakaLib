0c4f02bcf7e48eb42afc6ba3d553677a0555dcc8
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2017-06-15 10:51:02 +0200

    testing of failing test

---
+++ src/WakaWakaLib.Tests/Processing/Mapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+
+namespace WakaWakaLib.Tests.Processing
+{
+
+    public class Mapper
+    {
+
+        [Fact]
+        public void FromJson_User()
+        {
+
+            var s = Properties.Resources.UserDataJson;
+
+            var user = WakaWakaLib.Processing.Mapper.FromJson<Models.User>(s);
+
+            user.ShouldNotBeNull();
+                        
+            user.CreatedAt.ShouldBe(new DateTime(2001, 1, 1));
+            user.DisplayName.ShouldBe("display name");
+            user.Email.ShouldBe("email");
+            user.EmailPublic.ShouldBe(true);
+            user.FullName.ShouldBe("full name, testing of failing test");
+            user.HasPremiumFeatures.ShouldBe(true);
+            user.HumanReadableWebsite.ShouldBe("human readable website");
+            user.Id.ShouldBe("id");
+            user.IsEmailConfirmed.ShouldBe(true);
+            user.IsHireable.ShouldBe(true);
+            user.LanguagesUsedPublic.ShouldBe(true);
+            user.LastHeartbeat.ShouldBe(new DateTime(2001, 1, 1));
+            user.LastPlugin.ShouldBe("last plugin");
+            user.LastPluginName.ShouldBe("last plugin name");
+            user.LastProject.ShouldBe("last project");
+            user.Location.ShouldBe("location");
+            user.LoggedTimePublic.ShouldBe(true);
+            user.ModifiedAt.ShouldBe(new DateTime(2001, 1, 1));
+            user.Photo.ShouldBe("photo");
+            user.PhotoPublic.ShouldBe(true);
+            user.Plan.ShouldBe("plan");
+            user.Timezone.ShouldBe("timezone");
+            user.Username.ShouldBe("username");
+            user.Website.ShouldBe("website");
+            
+        }
+
+    }
+    
+}
